2e88b81f3f9d57bb00c19ec6cd2fa3430193808f
Somnath474 <somnathgupta474@gmail.com>
2026-04-27 23:46:28 +0530

    fixed backend issue / updated API / changed localhost

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://gen-ai-interview-report-generator-1.onrender.com",
     withCredentials: true
 })
 
